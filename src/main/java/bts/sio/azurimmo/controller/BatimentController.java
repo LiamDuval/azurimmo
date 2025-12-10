a980d163117d897bb14a0d46f79d5f8ab4426c4b
LiamDuval <liamd4047@gmail.com>
2025-12-10 11:27:46 +0100

    Swagger plus operateur et tag

--- src/main/java/bts/sio/azurimmo/controller/BatimentController.java
+++ src/main/java/bts/sio/azurimmo/controller/BatimentController.java
@@ -2,6 +2,8 @@ package bts.sio.azurimmo.controller;
 
 import bts.sio.azurimmo.model.dto.BatimentDTO;
 import bts.sio.azurimmo.service.BatimentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.Optional;
 
@@ -16,32 +18,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/batiments")
+@Tag(name = "Batiments", description = "Gestion des Batiments")
 public class BatimentController {
 
 
 	private final BatimentService batimentService;
 	
 	public BatimentController(BatimentService batimentService) {
-		this.batimentService = batimentService;
-		
+	    this.batimentService = batimentService;
 	}
-	
-	 @GetMapping("/{batimentId}")
-	 public Optional <BatimentDTO> getBatimentDTO(@PathVariable long batimentId) {
-	 return batimentService.getBatimentDTO(batimentId);
-	 }
-	 
-	 @GetMapping("/re/{batimentId}")
-	 public ResponseEntity<BatimentDTO> getBatimentReDTO(@PathVariable long batimentId) {
-	  return batimentService.getBatimentDTO(batimentId)
-	  .map(ResponseEntity::ok) // batiment trouvé → 200
-	  .orElse(ResponseEntity.notFound().build()); // pas trouvé → 404
-	 }
-	 
-	 @PostMapping("/")
-	 public ResponseEntity<BatimentDTO> createBatiment(@RequestBody BatimentDTO dto) {
-	 BatimentDTO savedDTO = batimentService.saveBatimentDTO(dto);
-	 return ResponseEntity.status(201).body(savedDTO); // 201 Created
-	 }
+
+	@GetMapping("/{batimentId}")
+	@Operation(summary = "Récupérer un batiment par son ID")
+	public Optional<BatimentDTO> getBatimentDTO(@PathVariable long batimentId) {
+	    return batimentService.getBatimentDTO(batimentId);
+	}
+
+	@GetMapping("/re/{batimentId}")
+	@Operation(summary = "Récupérer un batiment par son ID avec réponse HTTP 200 ou 404")
+	public ResponseEntity<BatimentDTO> getBatimentReDTO(@PathVariable long batimentId) {
+	    return batimentService.getBatimentDTO(batimentId)
+	            .map(ResponseEntity::ok)                 // 200 OK si trouvé
+	            .orElse(ResponseEntity.notFound().build()); // 404 si pas trouvé
+	}
+
+	@PostMapping("/")
+	@Operation(summary = "Créer un nouveau batiment")
+	public ResponseEntity<BatimentDTO> createBatiment(@RequestBody BatimentDTO dto) {
+	    BatimentDTO savedDTO = batimentService.saveBatimentDTO(dto);
+	    return ResponseEntity.status(201).body(savedDTO); // 201 Created
+	}
+
 
 }
