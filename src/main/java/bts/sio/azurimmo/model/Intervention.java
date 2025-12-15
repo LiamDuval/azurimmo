31caedc8911749c8d01f007903a6ea5620b4bc05
LiamDuval <liamd4047@gmail.com>
2025-12-15 14:25:32 +0100

    Ajout repository intervention

--- src/main/java/bts/sio/azurimmo/model/Intervention.java
+++ src/main/java/bts/sio/azurimmo/model/Intervention.java
@@ -1,10 +1,16 @@
 package bts.sio.azurimmo.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "Intervention")
 public class Intervention {
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
