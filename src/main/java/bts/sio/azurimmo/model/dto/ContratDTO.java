abbf6c5744ac9c9ae09f057b65e3e613c96aca45
LiamDuval <liamd4047@gmail.com>
2025-12-17 10:01:01 +0100

    Repository

--- src/main/java/bts/sio/azurimmo/model/dto/ContratDTO.java
+++ src/main/java/bts/sio/azurimmo/model/dto/ContratDTO.java
@@ -1,5 +1,14 @@
 package bts.sio.azurimmo.model.dto;
 
-public class ContratDTO {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class ContratDTO  {
+ private String dateDebut;
+ private String dateFin;
+ private String montantBrut;
+ private String montantCharge;
+ private String statut;
 }
