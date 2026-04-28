af15123b5f65d6bafa0a08dff5771ef8d1f83b60
LiamDuval <liamduval53@gmail.com>
2026-04-28 15:20:05 +0200

    corsConfig adress change

--- azurimmo-backend/src/main/java/bts/sio/azurimmo/CorsConfig.java
+++ azurimmo-backend/src/main/java/bts/sio/azurimmo/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://172.20.177.6:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
